7d0dffb3fafe7a34c86081716169101a1911cfbf
root <root@mfs55.qa.lab>
2016-08-26 04:10:07 +0000

    changed file names

---
+++ src/streams_util.h
@@ -0,0 +1,14 @@
+#ifndef STREAMS_UTIL_H
+#define STREAMS_UTIL_H
+
+#include "types.h"
+#include "streams.h"
+#include "streams_common.h"
+#include "streams_consumer.h"
+#include "streams_producer.h"
+
+
+
+#endif
+
+
---
+++ src/streams_wrapper.h
@@ -0,0 +1,21 @@
+#ifndef MARLIN_WRAPPER_H
+#define MARLIN_WRAPPER_H
+
+#include "rdkafka.h"
+
+typedef struct producer_callback_ctx {
+	        rd_kafka_t *rk;
+		        void *msg_opaque;
+			        rd_kafka_topic_t *topic;
+				        int msgflags;
+} streams_producer_callback_ctx;
+
+
+typedef struct consumer_callback_ctx {
+	        rd_kafka_t *rk;
+		        rd_kafka_topic_partition_list_t *partitions;
+} streams_consumer_callback_ctx;
+
+#endif
+
+
